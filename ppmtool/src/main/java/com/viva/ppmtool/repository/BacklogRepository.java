54746948cc0ad4c7d0963866363054075c9bf446
Haardik Sikka <hsikka95@gmail.com>
2020-07-26 13:38:34 -0700

    created backlog and project task repo

---
+++ ppmtool/src/main/java/com/viva/ppmtool/repository/BacklogRepository.java
@@ -0,0 +1,12 @@
+package com.viva.ppmtool.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.viva.ppmtool.domain.Backlog;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+
+    Backlog findByProjectIdentifier(String Identifier);
+}
